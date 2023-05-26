7e79582e2f531bbf4ebdd8632a40d90e450bf379
Sung <slee1070@my.bcit.ca>
2023-05-25 18:13:17 -0700

    Update chatbot.py and app.py for remote server

--- python_chatbot/app.py
+++ python_chatbot/app.py
@@ -1,6 +1,13 @@
+import os
+
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from chatbot import chat_bot
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 
 # Create the Flask app
 app = Flask(__name__)
@@ -21,9 +28,15 @@ def chat():
     """
     Handles the chat request and returns the chatbot response.
     """
-    query = request.json['query']
-    response = chat_bot(query)
-    return jsonify({'response': response})
+    password = request.json['password']
+    if password == os.getenv("CHATBOT_REMOTE_SERVER_PASSWORD"):
+        query = request.json['query']
+        persona_prompt = request.json['personaPrompt']
+        example_conversation = request.json['exampleConversation']
+        response = chat_bot(query, persona_prompt, example_conversation)
+        return jsonify({'response': response})
+    else:
+        return "Forbidden", 403
 
 
 if __name__ == '__main__':
